1ed4ab385307313a572d86fe2a727c05eb5c4c9b
Ilya Buglakov <neonlightnight@gmail.com>
2022-06-12 09:18:23 +0300

    initial-commit

---
+++ src/test/java/com/ilboogl/certy/CertyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ilboogl.certy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CertyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
